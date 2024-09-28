a09060a79be437cc0708755096a136dc1e55731e
Jost Triller <tsoj.tsoj@gmx.de>
2024-09-28 21:56:02 +0200

    Added data parsing and processing stuff

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ```bash
 conda create -n finetuning python=3.12
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
-pip install transformers datasets peft bitsandbytes
+pip install transformers datasets peft bitsandbytes dataclasses-json
 ```
---
+++ data.py
@@ -0,0 +1,178 @@
+from dataclasses import dataclass, field
+from dataclasses_json import dataclass_json, config, cfg
+from datetime import datetime
+from typing import List, Optional
+from pathlib import Path
+
+cfg.global_config.encoders[datetime] = datetime.isoformat
+cfg.global_config.decoders[datetime] = datetime.fromisoformat
+
+@dataclass_json
+@dataclass
+class Guild:
+    id: str
+    name: str
+    iconUrl: str
+
+@dataclass_json
+@dataclass
+class Channel:
+    id: str
+    type: str
+    categoryId: str
+    category: str
+    name: str
+    topic: Optional[str]
+
+@dataclass_json
+@dataclass
+class DataRange:
+    after: Optional[datetime]
+    before: Optional[datetime]
+
+@dataclass_json
+@dataclass
+class Attachment:
+    id: str
+    url: str
+    fileName: str
+    fileSizeBytes: int
+
+@dataclass_json
+@dataclass
+class Image:
+    url: str
+    width: int
+    height: int
+
+@dataclass_json
+@dataclass
+class Field:
+    name: str
+    value: str
+    isInline: bool
+
+@dataclass_json
+@dataclass
+class Embed:
+    title: str
+    url: Optional[str]
+    timestamp: Optional[datetime]
+    description: str
+    images: List[Image]
+    fields: List[Field]
+    thumbnail: Optional[Image] = field(default=None)
+
+@dataclass_json
+@dataclass
+class Sticker:
+    id: str
+    name: str
+    format: str
+    sourceUrl: str
+
+@dataclass_json
+@dataclass
+class Emoji:
+    id: str
+    name: str
+    code: str
+    isAnimated: bool
+    imageUrl: str
+
+@dataclass_json
+@dataclass
+class Role:
+    id: str
+    name: str
+    color: Optional[str]
+    position: int
+
+@dataclass_json
+@dataclass
+class User:
+    id: str
+    name: str
+    discriminator: str
+    nickname: str
+    isBot: bool
+    avatarUrl: str
+    color: Optional[str]  = field(default=None)
+    roles: Optional[List[Role]]  = field(default=None)
+
+@dataclass_json
+@dataclass
+class Reaction:
+    emoji: Emoji
+    count: int
+    users: List[User]
+
+@dataclass_json
+@dataclass
+class Reference:
+    messageId: Optional[str]
+    channelId: str
+    guildId: Optional[str]
+
+@dataclass_json
+@dataclass
+class Message:
+    id: str
+    type: str
+    timestamp: datetime
+    timestampEdited: Optional[datetime]
+    callEndedTimestamp: Optional[datetime]
+    isPinned: bool
+    content: str
+    author: User
+    attachments: List[Attachment]
+    embeds: List[Embed]
+    stickers: List[Sticker]
+    reactions: List[Reaction]
+    mentions: List[User]
+    reference: Optional[Reference] = field(default=None)
+
+@dataclass_json
+@dataclass
+class Chat:
+    guild: Guild
+    channel: Channel
+    dateRange: DataRange
+    exportedAt: datetime
+    messages: List[Message]
+    messageCount: int
+
+
+in_path = Path("data/engine_programming_discord")
+out_path = Path("data/text")
+
+for file_path in in_path.glob("*.json"):
+    print("Loading from", file_path)
+
+    with open(file_path, 'r') as file:
+
+        id_to_message = {}
+
+        chat = Chat.from_json(file.read())
+
+        chat_text = ""
+        for message in chat.messages:
+            assert message.id not in id_to_message
+            id_to_message[message.id] = message
+
+            chat_text += "<|" + message.author.name
+            if message.type == "Reply" and message.reference.messageId in id_to_message:
+                previous_message = id_to_message[message.reference.messageId]
+                chat_text += " replies to " + previous_message.author.name
+
+            chat_text += "|>\n"
+            chat_text += message.content
+            chat_text += "</s>\n\n"
+
+        file_name = out_path / (chat.guild.name + " - " + chat.channel.name + ".txt")
+
+        with open(file_name, 'w') as file:
+            # Write text to the file
+            file.write(chat_text)
+            print("Finished", file_name)
+        # for
