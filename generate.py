147e32fe70184ff66a5dc6927526518416e90322
Jost Triller <tsoj.tsoj@gmx.de>
2024-10-03 02:22:31 +0200

    more features for the generator

--- generate.py
+++ generate.py
@@ -16,7 +16,7 @@ from transformers import (
 )
 from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model, PeftModel
 from pathlib import Path
-
+import sys
 from accelerate import Accelerator
 from transformers.generation.configuration_utils import GENERATION_CONFIG_NAME
 
@@ -51,7 +51,7 @@ class MyStoppingCriteria(StoppingCriteria):
 
 # Text generation setup
 @torch.no_grad()
-def generate_text(model, tokenizer, prompt, max_new_tokens=1000, print_while_generating=False):
+def generate_text(model, tokenizer, prompt, max_new_tokens=1000, interactive=False):
 
     output = prompt
 
@@ -75,43 +75,75 @@ def generate_text(model, tokenizer, prompt, max_new_tokens=1000, print_while_gen
 
     current_config = "normal" if prompt.count("|>") >= prompt.count("<|") else "user"
 
-    if print_while_generating:
-        print("----------------------")
+    print("----------------------")
     first_iteration = True
+    skipping = False
     while True:
 
-        inputs = tokenizer(output, return_tensors="pt").to(model.device)
+        if interactive and current_config == "user" and not skipping:
+            assert len(output) >= 3 and output[-3:] == "\n<|", output
 
-        streamer = TextStreamerWithNoNewline(tokenizer, skip_prompt=not first_iteration) if print_while_generating else None
-        first_iteration = False
+            sys.stdout.write("\r\033[K")
+            sys.stdout.write("Next user ('a' for auto mode, 's' to skip, 'q' to quit): ")
+            sys.stdout.flush()
 
-        #print("Using config:", current_config)
-        model_output = model.generate(
-            **inputs,
-            streamer=streamer,
-            generation_config=configs[current_config],
-            max_length=None,
-            max_new_tokens=max_new_tokens,
-            pad_token_id=tokenizer.eos_token_id,
-            stopping_criteria=[MyStoppingCriteria(stops[current_config], output, tokenizer)],
-        )
+            # Get user input
+            user_input = input()
 
-        output = tokenizer.decode(model_output[0], skip_special_tokens=True)
+            # Erase the line
+            sys.stdout.write("\033[1A")  # Move cursor up one line
+            sys.stdout.write("\033[K")   # Erase the line
+            sys.stdout.flush()
 
-        #print("Stopped at:", output, "\n-----------------------")
+            if user_input == "q":
+                break
+            if user_input in ["a", "s"]:
+                sys.stdout.write("<|")
+                sys.stdout.flush()
+                if user_input == "a":
+                    interactive = False
+                if user_input == "s":
+                    skipping = True
+                continue
+
+            new_output = user_input.split()[0]
+            sys.stdout.write("<|" + new_output)
+            sys.stdout.flush()
+            output += new_output
+        else:
+            inputs = tokenizer(output, return_tensors="pt").to(model.device)
+
+            streamer = TextStreamerWithNoNewline(tokenizer, skip_prompt=not first_iteration)
+            first_iteration = False
+
+
+            #print("Using config:", current_config)
+            model_output = model.generate(
+                **inputs,
+                streamer=streamer,
+                generation_config=configs[current_config],
+                max_length=None,
+                max_new_tokens=max_new_tokens,
+                pad_token_id=tokenizer.eos_token_id,
+                stopping_criteria=[MyStoppingCriteria(stops[current_config], output, tokenizer)],
+            )
+
+            previous_output = output
+            output = tokenizer.decode(model_output[0], skip_special_tokens=True)
+
+            #print("Stopped at:", output, "\n-----------------------")
+
+            num_new_generated_tokens = len(model_output[0]) - len(tokenizer.encode(previous_output))
+            num_total_generated_tokens = len(model_output[0]) - len(tokenizer.encode(prompt))
+
+            if (num_new_generated_tokens if interactive else num_total_generated_tokens) >= max_new_tokens:
+                break
 
         current_config = "normal" if current_config == "user" else "user"
+        skipping = False
 
-        num_generated_tokens = len(model_output[0]) - len(tokenizer.encode(prompt))
 
-        if not print_while_generating:
-            print(f"Generated {num_generated_tokens}/{max_new_tokens} tokens")
-
-        if num_generated_tokens >= max_new_tokens:
-            break
-
-    if print_while_generating:
-        print("\n----------------------")
+    print("\n----------------------")
 
     return output
 
@@ -135,5 +167,17 @@ model = AutoModelForCausalLM.from_pretrained(
 # Generate text
 prompt = "Stockfish - engines-dev:\n"
 
-generated_text = generate_text(model, tokenizer, prompt, print_while_generating=True)
-#print(f"Generated text:\n{generated_text}")
+interactive = "interactive" in sys.argv[1:]
+max_new_tokens = 1000
+for arg in sys.argv[1:]:
+    if arg.isdigit():
+        max_new_tokens = int(arg)
+    elif arg != "interactive":
+        prompt = arg
+
+print("interactive:", interactive)
+print("max_new_tokens:", max_new_tokens)
+print(f"prompt: \"{prompt}\"")
+
+
+generate_text(model, tokenizer, prompt, max_new_tokens=max_new_tokens, interactive=interactive)
