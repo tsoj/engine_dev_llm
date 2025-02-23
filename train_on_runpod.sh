cb530f4d68a1b524fd31f354c29d1608d0692c9e
Jost Triller <tsoj.tsoj@gmx.de>
2025-02-23 16:21:32 +0100

    Stuff

---
+++ train_on_runpod.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+cd /workspace
+
+apt update && apt install nano nvtop
+
+export HF_HOME=/workspace/hf_cache/
+
+git clone https://github.com/tsoj/engine_dev_llm.git
+cd engine_dev_llm
+git switch more_data
+mkdir data
+
+pip install torch
+pip install transformers datasets peft bitsandbytes dataclasses-json
+
+while true; do
+    read -p "Updated constants.py? (y/n): " answer
+
+    if [[ "$answer" =~ ^[yY]([eE][sS])?$ ]]; then
+        break
+    fi
+done
+
+while true; do
+    read -p "Moved all the data via 'scp -r -P <your_port> /path/to/data/text/ root@<your_ip>:/workspace/engine_dev_llm/data/'? (y/n): " answer
+
+    if [[ "$answer" =~ ^[yY]([eE][sS])?$ ]]; then
+        break
+    fi
+done
+
+echo "Starting training"
+
+nohup python train.py > nohup.out &
+tail -f nohup.out
