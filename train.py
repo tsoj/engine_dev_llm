d11cf7b0bef0ab785dfa308d6ee4b952842bb0b2
Jost Triller <tsoj.tsoj@gmx.de>
2025-02-26 01:49:31 +0100

    Undid unquant

--- train.py
+++ train.py
@@ -45,16 +45,16 @@ tokenizer = AutoTokenizer.from_pretrained(constants.model_name, token=constants.
 tokenizer.pad_token = tokenizer.eos_token
 
 
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_quant_type="nf4",
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_compute_dtype=torch.bfloat16,
-# )
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
 
 model = AutoModelForCausalLM.from_pretrained(
     constants.model_name,
-    # quantization_config=bnb_config,
+    quantization_config=bnb_config,
     device_map="auto",
     token=constants.token,
 )
