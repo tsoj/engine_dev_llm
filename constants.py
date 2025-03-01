85191983f0c70615688e3fc9562f6163e1e7c3ba
Jost Triller <tsoj.tsoj@gmx.de>
2025-03-01 03:30:13 +0100

    Updated constants

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 
 max_character_context_length=4096
-max_token_context_length=1024
+max_token_context_length=1536
 max_single_message=700
-model_name = "Qwen/Qwen2.5-0.5B"
+model_name = "mistralai/Mistral-Small-24B-Base-2501"
 token = None
